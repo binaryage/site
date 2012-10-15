802b425da3063e88b8b4c7668fb7c6c665fe8c76
Antonin Hildebrand <antonin@hildebrand.cz>
2012-10-15 04:13:09 +0200

    exit with error when closure compiler fails

--- .jekyll-plugins/js_combinator.rb
+++ .jekyll-plugins/js_combinator.rb
@@ -31,6 +31,7 @@ module Jekyll
           end
         rescue => e
           STDERR.puts "Closure Compiler Exception: #{e.message}"
+          raise FatalException.new('Closure Compiler: #{e.message}')
         end
 
         true
